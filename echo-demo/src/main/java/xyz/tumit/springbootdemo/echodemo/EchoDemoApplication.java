a54d9cba167b3d13d8d053dc1e4913492c5e24d2
tumit <tumit@live.com>
2019-03-07 03:15:34 +0700

    init echo-demo

---
+++ echo-demo/src/main/java/xyz/tumit/springbootdemo/echodemo/EchoDemoApplication.java
@@ -0,0 +1,13 @@
+package xyz.tumit.springbootdemo.echodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoDemoApplication.class, args);
+	}
+
+}
